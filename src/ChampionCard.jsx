cff50ca9b363e6f1a5833ebf3a3c694c60e8b734
Laboratorio Unidade Fatima <labunivas@univas.edu.br>
2025-05-20 22:03:05 -0300

    Infos campeao

--- src/ChampionCard.jsx
+++ src/ChampionCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 
 function ChampionCard({ champion, onClick }) {
-  const { id, name, title } = champion;
+  const { id, name, title, key, info, stats } = champion;
   const imageUrl = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${id}_0.jpg`;
 
   return (
@@ -11,6 +11,16 @@ function ChampionCard({ champion, onClick }) {
       <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">{title}</Card.Subtitle>
+        <Card.Text style = {{ fontSize: '0.85rem'}}>
+          <strong>ID Numérico{ key }</strong>
+          <strong>Ataque: {info.attack}</strong>
+          <strong>Defesa: {info.defense}</strong>
+          <strong>Magia: {info.magic}</strong>
+          <strong>Dificuldade: {info.difficulty}</strong>
+          <strong>Vida base: {stats.hp}</strong>
+          <strong>Dano base: {stats.attackdamage}</strong>
+          <strong>Velcoidade: {stats.movespeed}</strong>
+        </Card.Text>
       </Card.Body>
     </Card>
   );
